6fc98ef7b3f969f2b936d68d363a3403398c2101
Bryan Muller <starwarsgeek0975@gmail.com>
2018-01-23 14:53:12 -0700

    created template landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import '../stylesheets/App.css';
 import {Nav} from "./Nav";
+import {SearchBar} from "./SearchBar";
 
 class App extends Component {
 
-  render() {
-    return (
-        <main className="App">
-          <Nav />
-        </main>
-    );
-  }
+    render() {
+        return (
+            <main className="App">
+                <Nav/>
+                <SearchBar/>
+            </main>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../stylesheets/SearchBar.css';
+
+export class SearchBar extends Component {
+    render() {
+        return (
+            <div className='BtnWrapper'>
+            <div className='SearchWrapper animated fadeIn'>
+                <input type='text' placeholder='What would you like to find?'/>
+            </div>
+                <button>Search</button>
+            </div>
+        )
+    }
+}
