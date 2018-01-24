c90aadcdda3cfff503ecd882f8d22bb2d818c1fc
Bryan <starwarsgeek0975@gmail.com>
2018-01-24 11:38:41 -0700

    created flexbox grid layout

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,14 +2,28 @@ import React, {Component} from 'react';
 import '../stylesheets/SearchBar.css';
 
 export class SearchBar extends Component {
-    render() {
-        return (
-            <div className='BtnWrapper'>
-            <div className='SearchWrapper animated fadeIn'>
-                <input type='text' placeholder='What would you like to find?'/>
-            </div>
-                <button>search</button>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {searchBtnClass: 'btn-state-initial'}
+  }
+
+  render() {
+    const comp = `SearchBar`;
+    const inputWrapClasses = `flex-grid SearchWrapper animated fadeIn`;
+    const inputClasses = `col`;
+    const btnWrapClasses = `flex-grid`;
+    const btnClasses = `${this.state.searchBtnClass} col`;
+
+    return (
+        <div className={comp}>
+          <div className={inputWrapClasses}>
+            <input className={inputClasses} type='text' placeholder='What would you like to find?'/>
+          </div>
+          <div className={btnWrapClasses}>
+            <div className='col-grow-6'>{/*For Spacing */}</div>
+            <button className={btnClasses}>search</button>
+          </div>
+        </div>
+    )
+  }
 }
