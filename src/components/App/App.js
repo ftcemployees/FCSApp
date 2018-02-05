4e14ea5bedf8e0f49563d5b2bcac4ead2d708a29
Bryan <starwarsgeek0975@gmail.com>
2018-02-05 15:01:48 -0700

    initial revamp commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {} from 'antd';
+import {SearchBar} from "../SearchBar/SearchBar";
+
+export const App = () => {
+
+
+  return (
+      <div id={'wrapper'}>
+        <div>Hello</div>
+        <SearchBar/>
+      </div>
+  );
+};
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Input} from 'antd';
+
+const Search = Input.Search;
+
+export const SearchBar = () => {
+  const SearchMessages = [
+    "What would you like to find?",
+    "Search by name, keyword, or tag",
+    "Try searching for 'Leopard Print'",
+    "Are you ready to discover something old?",
+    //TODO -SearchMessages -- You may want to change this last one
+    "If you are reading this, please help! I am trapped and I can't get out!!"
+  ];
+
+  return (
+      <Search placeholder={SearchMessages[Math.floor(Math.random() * SearchMessages.length)]} enterButton="Search"
+                  size="large"/>
+  )
+};
